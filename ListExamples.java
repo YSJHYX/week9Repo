675be166dda1abd81ed44ea94a2ff7dabbf0e961
Yuxiang Hong <89092053+YSJHYX@users.noreply.github.com>
2024-05-28 11:52:57 -0700

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -19,7 +19,7 @@ class ListExamples {
   }
 
 
-  Takes two sorted list of strings (so "a" appears before "b" and so on),
+  // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
   static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
